e3552907a25890586b640209d065c9d9d9637562
Martin <MartinGiroux3>
2020-05-20 14:10:07 -0400

    update tables

--- script_creation_DB.sql
+++ script_creation_DB.sql
@@ -8,24 +8,13 @@ CREATE TABLE examen
 	PRIMARY KEY (id_cours_examen)
 );
 
-CREATE TABLE local
-(
-	numero_local VARCHAR(64) NOT NULL,
-	nbr_places INT NOT NULL,
-	id_cours_examen VARCHAR(64) NOT NULL,
-	PRIMARY KEY (numero_local),
-	FOREIGN KEY (id_cours_examen) REFERENCES examen(id_cours_examen)
-);
-
 CREATE TABLE etudiant
 (
 	cip CHAR(8) NOT NULL,
 	nom VARCHAR(64) NOT NULL,
 	prenom VARCHAR(64) NOT NULL,
-	presence BOOL NOT NULL,
-	id_cours_examen VARCHAR(64),
 	PRIMARY KEY(cip),
-	FOREIGN KEY (id_cours_examen) REFERENCES examen(id_cours_examen),
+	FOREIGN KEY (id_cours_examen) REFERENCES examen(id_cours_examen)
 );
 
 CREATE TABLE place
@@ -33,8 +22,9 @@ CREATE TABLE place
 	numero_chaise INT NOT NULL,
 	numero_local VARCHAR(64) NOT NULL,
 	cip CHAR(8),
+	presence BOOL,
+	id_cours_examen VARCHAR(64),
 	PRIMARY KEY (numero_chaise, numero_local),
-	FOREIGN KEY (numero_local) REFERENCES local(numero_local),
 	FOREIGN KEY (cip) REFERENCES etudiant(cip)
 );
 
