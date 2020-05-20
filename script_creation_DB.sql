03f4186043bd972c7decc391a01b13add3352b53
Martin <MartinGiroux3>
2020-05-20 10:27:11 -0400

    update ajout script creation

---
+++ script_creation_DB.sql
@@ -0,0 +1,57 @@
+CREATE TABLE examen
+(
+	id_cours_examen VARCHAR(64) NOT NULL,
+	nbr_etudiants_prevu INT NOT NULL,
+	nbr_etudiants_presents INT NOT NULL,
+	debut TIME NOT NULL,
+	fin TIME NOT NULL,
+	PRIMARY KEY (id_cours_examen)
+);
+
+CREATE TABLE place
+(
+	numero_chaise INT NOT NULL,
+	libre_ou_non BOOL NOT NULL,
+	PRIMARY KEY (numero_chaise)
+);
+
+CREATE TABLE local
+(
+	numero_local VARCHAR(64) NOT NULL,
+	nbr_places INT NOT NULL,
+	id_cours_examen VARCHAR(64) NOT NULL,
+	PRIMARY KEY (numero_local),
+	FOREIGN KEY (id_cours_examen) REFERENCES examen(id_cours_examen)
+);
+
+CREATE TABLE etudiant
+(
+	cip CHAR(8) NOT NULL,
+	nom VARCHAR(64) NOT NULL,
+	prenom VARCHAR(64) NOT NULL,
+	presence BOOL NOT NULL,
+	id_cours_examen VARCHAR(64),
+	numero_chaise INT NOT NULL,
+	PRIMARY KEY(cip),
+	FOREIGN KEY (id_cours_examen) REFERENCES examen(id_cours_examen),
+	FOREIGN KEY (numero_chaise) REFERENCES place(numero_chaise)
+);
+
+CREATE TABLE sorties
+(
+	id_sortie INT NOT NULL,
+	heure_debut TIME NOT NULL,
+	heure_fin TIME NOT NULL,
+	cip CHAR(8) NOT NULL,
+	PRIMARY KEY (id_sortie),
+	FOREIGN KEY (cip) REFERENCES etudiant(cip)
+);
+
+CREATE TABLE constitue_de
+(
+	numero_local CHARCHAR(64) NOT NULL,
+	numero_chaise INT NOT NULL,
+	PRIMARY KEY (numero_local, numero_chaise),
+	FOREIGN KEY (numero_local) REFERENCES local(numero_local),
+	FOREIGN KEY (numero_chaise) REFERENCES place(numero_chaise
+);
